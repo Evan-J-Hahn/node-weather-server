cea3c2d315305c35c3d57acfbd9cfb0bf7fbc28c
Evan-J-Hahn <evan.hahn@me.com>
2020-05-15 14:40:47 -0700

    Import 7day forecast widget

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + '°F out. With a high of ' + body.daily.data[0].temperatureHigh + '°F, and a low of ' + body.daily.data[0].temperatureLow + '°F. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature.toFixed(0) + '°F out. With a high of ' + body.daily.data[0].temperatureHigh.toFixed(0) + '°F, and a low of ' + body.daily.data[0].temperatureLow.toFixed(0) + '°F. There is a ' + body.currently.precipProbability.toFixed(0) + '% chance of rain.')
         }
     })
 }
